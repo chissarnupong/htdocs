786deeaa2cfb8382cde1123d06ebf38d5330760b
chissarnupong <31725669+chissarnupong@users.noreply.github.com>
2018-10-31 13:25:07 +0700

    up1

---
+++ upload/upfile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<!-- enctype="multipart/form-data"
+id="fileToUpload"
+value="Upload" name="submit"
+-->
+<form action="upload.php" method="post" >
+    Select pcap to upload:
+    <input type="file" name="fileToUpload">
+    <input type="submit" >
+</form>
+
+</body>
+</html>
